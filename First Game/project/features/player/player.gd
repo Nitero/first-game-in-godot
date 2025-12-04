ba6e8de07f51f0cea07497e0ce74d31bd017affc
Nito <nikolas1997@hotmail.de>
2025-12-04 23:32:05 +0100

    better player movement
    - horizontal: acceleration, deceleration, turn speed
    - vertical: variable jump height, jump buffer, coyote time

--- First Game/project/features/player/player.gd
+++ First Game/project/features/player/player.gd
@@ -1,36 +1,134 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const GROUND_MAX_RUN_SPEED = 115
+
+const GROUND_ACCELERATION = 500
+const GROUND_DECELERATION = 500
+const GROUND_TURN_SPEED = 800
+
+const AIR_ACCELERATION = 500
+const AIR_DECELERATION = 500
+const AIR_TURN_SPEED = 400
+
+const JUMP_HEIGHT = 110
+const TIME_TO_JUMP_APEX = 1
+const UP_MOVEMENT_MULTIPLIER = 0.5
+const DOWN_MOVEMENT_MULTIPLIER = 1.25
+const JUMP_CUTOFF = 1.5
+
+const FALL_SPEED_LIMIT = 350
+const JUMP_SPEED_LIMIT = INF
+
+const JUMP_BUFFER = 0.2
+const COYOTE_TIME = 0.2
+
+const EPSILON = 0.01
+const DEFAULT_GRAVITY_MULTIPLIER = 1
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var look_direction: float
+var desired_velocity: Vector2
+
+var is_jump_desired: bool
+var is_jump_pressed: bool
+var is_jump_ongoing: bool
+var jump_buffer_timer: float
+
+var coyote_time_timer: float
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(_delta):
+func _input(_event):
+	look_direction = Input.get_axis("move_left", "move_right")#-1 | 0 | 1
+	desired_velocity = Vector2(look_direction, 0) * max(0, GROUND_MAX_RUN_SPEED)
+	if Input.is_action_just_pressed("jump"):
+		is_jump_desired = true
+	is_jump_pressed = Input.is_action_pressed("jump")
+	
+func _process(delta):
+	poll_jump_buffer(delta)
+	poll_coyote_time(delta)
+	flip_sprite_to_look_direction()
 	play_animation()
-	
+
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("move_left", "move_right")#-1 | 0 | 1
-	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	move_horizontally(delta)
+	move_vertically(delta)
 	move_and_slide()
+
+func move_horizontally(delta):
+	var acceleration = GROUND_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	var deceleration = GROUND_DECELERATION if is_on_floor() else AIR_DECELERATION
+	var turn_speed = GROUND_TURN_SPEED if is_on_floor() else AIR_TURN_SPEED
+
+	var change_speed
+
+	if look_direction != 0:
+		if look_direction != sign(velocity.x):
+			change_speed = turn_speed
+		else:
+			change_speed = acceleration
+	else:
+		change_speed = deceleration
+
+	velocity.x = move_toward(velocity.x, desired_velocity.x, change_speed * delta)
+
+func poll_jump_buffer(delta):
+	if JUMP_BUFFER <= 0 || !is_jump_desired:
+		return
+	jump_buffer_timer += delta
+	if jump_buffer_timer > JUMP_BUFFER:
+		is_jump_desired = false
+		jump_buffer_timer = 0
+
+func poll_coyote_time(delta):
+	if !is_jump_ongoing && !is_on_floor():
+		coyote_time_timer += delta
+	else:
+		coyote_time_timer = 0
+
+func move_vertically(delta):
+	if is_on_floor():
+		is_jump_ongoing = false
+
+	var gravity_multiplier = get_gravity_multiplier()
+	if is_jump_desired:
+		execute_jump(gravity_multiplier)
+	if !is_on_floor():
+		velocity.y += gravity * gravity_multiplier * delta
+
+	velocity.y = clamp(velocity.y, -JUMP_SPEED_LIMIT, FALL_SPEED_LIMIT)
+
+func execute_jump(gravity_multiplier):
+	if is_on_floor() || (coyote_time_timer > EPSILON && coyote_time_timer < COYOTE_TIME):
+		is_jump_ongoing = true
+		is_jump_desired = false
+		jump_buffer_timer = 0
+		coyote_time_timer = 0
+
+		var new_gravity = (2.0 * JUMP_HEIGHT) / (TIME_TO_JUMP_APEX * TIME_TO_JUMP_APEX)
+		var gravity_scale = (new_gravity / gravity) * gravity_multiplier
+
+		var jump_speed = sqrt(2.0 * gravity * gravity_scale * JUMP_HEIGHT)
+		velocity.y -= jump_speed
+
+func get_gravity_multiplier():
+	var gravity_multiplier = DEFAULT_GRAVITY_MULTIPLIER
+	if velocity.y < -EPSILON && !is_on_floor():
+		if is_jump_pressed && is_jump_ongoing:
+			gravity_multiplier = UP_MOVEMENT_MULTIPLIER
+		else:
+			gravity_multiplier = JUMP_CUTOFF
+	elif velocity.y > EPSILON && !is_on_floor():
+		gravity_multiplier = DOWN_MOVEMENT_MULTIPLIER
+	return gravity_multiplier
+
+func flip_sprite_to_look_direction():
+	if look_direction > 0:
+		animated_sprite.flip_h = false
+	elif look_direction < 0:
+		animated_sprite.flip_h = true
 
 func play_animation():
 	if is_on_floor():
